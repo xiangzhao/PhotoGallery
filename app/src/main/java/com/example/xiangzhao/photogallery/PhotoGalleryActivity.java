537ec5ab549c0f6dc3b5517003da7704cf3b47b6
Xiang Zhao <xiang.umass@gmail.com>
2014-11-12 17:29:29 -0800

    Initial commit

---
+++ app/src/main/java/com/example/xiangzhao/photogallery/PhotoGalleryActivity.java
@@ -0,0 +1,19 @@
+package com.example.xiangzhao.photogallery;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class PhotoGalleryActivity extends SingleFragmentActivity {
+
+
+    @Override
+    protected Fragment createFragment() {
+        return new PhotoGalleryFragment();
+    }
+
+
+}
