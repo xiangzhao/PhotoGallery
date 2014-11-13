aa41cb7fafe5bc2fac06248ba344bad460ab9042
Xiang Zhao <xiang.umass@gmail.com>
2014-11-13 11:47:02 -0800

    Use HandlerThread to download images and render them in the main thred.

--- app/src/main/java/com/example/xiangzhao/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/example/xiangzhao/photogallery/PhotoGalleryFragment.java
@@ -1,14 +1,18 @@
 package com.example.xiangzhao.photogallery;
 
+import android.graphics.Bitmap;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
+import android.widget.ImageView;
 
 import java.util.ArrayList;
 
@@ -17,23 +21,12 @@ import java.util.ArrayList;
  */
 public class PhotoGalleryFragment extends Fragment {
 
+    private static final String TAG = "PhotoGalleryFragment";
+
     GridView mGridView;
     ArrayList<GalleryItem> mItems;
 
-    private class FetchItemsTask extends AsyncTask<Void, Void, ArrayList<GalleryItem>> {
-
-        @Override
-        protected ArrayList<GalleryItem> doInBackground(Void... params) {
-            return new FlickrFetchr().fetchItems();
-        }
-
-        @Override
-        protected void onPostExecute(ArrayList<GalleryItem> galleryItems) {
-            mItems = galleryItems;
-            setupAdapter();
-        }
-    }
-
+    ThumbnailDownloader<ImageView> mThumbnailThread;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -42,6 +35,18 @@ public class PhotoGalleryFragment extends Fragment {
         setRetainInstance(true);
 
         new FetchItemsTask().execute();
+
+        mThumbnailThread = new ThumbnailDownloader<ImageView>(new Handler());
+        mThumbnailThread.setListener(new ThumbnailDownloader.Listener<ImageView>() {
+            @Override
+            public void onThumbnailDownloaded(ImageView imageView, Bitmap thumbnail) {
+                if (isVisible())
+                    imageView.setImageBitmap(thumbnail);
+            }
+        });
+        mThumbnailThread.start();
+        mThumbnailThread.getLooper();
+        Log.i(TAG, "Background thread started");
     }
 
     @Override
@@ -57,12 +62,59 @@ public class PhotoGalleryFragment extends Fragment {
         if (getActivity() == null || mGridView == null) return;
 
         if (mItems != null) {
-            mGridView.setAdapter(new ArrayAdapter<GalleryItem>(getActivity(),
-                    android.R.layout.simple_gallery_item, mItems));
+            mGridView.setAdapter(new GalleryItemAdapter(mItems));
         } else {
             mGridView.setAdapter(null);
         }
 
+
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        mThumbnailThread.clearQueue();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mThumbnailThread.quit();
+        Log.i(TAG, "Background thread destroyed");
+    }
+
+    private class FetchItemsTask extends AsyncTask<Void, Void, ArrayList<GalleryItem>> {
+
+        @Override
+        protected ArrayList<GalleryItem> doInBackground(Void... params) {
+            return new FlickrFetchr().fetchItems();
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<GalleryItem> galleryItems) {
+            mItems = galleryItems;
+            setupAdapter();
+        }
+    }
+
+    private class GalleryItemAdapter extends ArrayAdapter<GalleryItem> {
+        public GalleryItemAdapter(ArrayList<GalleryItem> items) {
+            super(getActivity(), 0, items);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                convertView = getActivity().getLayoutInflater()
+                        .inflate(R.layout.gallery_item, parent, false);
+            }
+            ImageView imageView = (ImageView) convertView.findViewById(R.id.gallery_item_imageView);
+            imageView.setImageResource(R.drawable.brian_up_close);
+            GalleryItem item = getItem(position);
+            mThumbnailThread.queueThumbnail(imageView, item.getUrl());
+
+            return convertView;
+        }
 
     }
 }
